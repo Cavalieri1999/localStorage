ffe85dfe4793f8a1e027390c0fb93ba5641575c8
Cavalieri1999 <cavalierimatheus325@gmail.com>
2024-11-13 09:56:24 -0300

    primeiraTela

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+
+//Criando variaveis para armazenar dados digitados
+var nome = document.querySelector('#nome')
+var email = document.querySelector('#email')
+var telefone = document.querySelector('#tel')
+var cpf = document.querySelector('#cpf')
+var senha = document.querySelector('#senha')
+
+
+//Exibindo MODAL
+var buttonModal = document.querySelector('#modal')
+var modal = document.querySelector('.telaDeCadastro')
+buttonModal.addEventListener('click', () => {
+    modal.showModal();
+})
+
+//Armazenando dados no LocalStorage
+
+var submit = document.querySelector('.enviarDados')
+
+submit.addEventListener('click', ()=>{
+    localStorage.setItem('nome', nome.value)
+    localStorage.setItem('email', email.value)
+    localStorage.setItem('telefone', telefone.value)
+    localStorage.setItem('cpf', cpf.value)
+
+})
